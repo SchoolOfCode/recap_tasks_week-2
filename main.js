893cdf1d90e18b726920acd0ec84e2c98fd741f0
unknown <taosharma@gmail.com>
2020-10-02 18:10:58 +0100

    finished tasks planning

---
+++ main.js
@@ -0,0 +1,6 @@
+/* Task 2.1 -  In the task-2 index.html there is a p tag that contains a counter. Using .setInterval(), have the counter increment
+ once every second when the page is loaded. */
+
+/* Task 2.2 - Save the above interval in a variable named 'intervalID' */
+
+/* Task 2.3 - Using .setTimeout() and .clearInterval(), stop the counter from incrementing after 12 seconds have passed. */
